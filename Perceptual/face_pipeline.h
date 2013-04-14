c95a41d470702c1c085c919c1d17f39673a4bb28
lfway <i.kalit@yandex.ru>
2013-04-14 18:03:48 +0400

    Draw rect around the face

--- Perceptual/face_pipeline.h
+++ Perceptual/face_pipeline.h
@@ -22,8 +22,15 @@ public:
 		// face
 		PXCFaceAnalysis *faceAnalyzer = QueryFace();
 		PXCFaceAnalysis::Landmark *landmark = faceAnalyzer->DynamicCast<PXCFaceAnalysis::Landmark>();
+		
 		// loop all faces
 		m_face_render->ClearData();
+
+		// face analyser
+		// Create detector instance
+		PXCFaceAnalysis::Detection *faceDetector=faceAnalyzer->DynamicCast<PXCFaceAnalysis::Detection>();
+		if(!faceDetector) return false;
+		
 		for(int fidx = 0; ; fidx++)
 		{
 			pxcUID fid = 0;
@@ -32,7 +39,15 @@ public:
 			if(sts < PXC_STATUS_NO_ERROR)
 				break;
 			// no more faces
+
+			// rect around the face
+			PXCFaceAnalysis::Detection::Data face_data;
+			faceDetector->QueryData(fid, &face_data);
+			m_face_render->SetDetectionData(&face_data);
+
+			// landmarks
 			m_face_render->SetLandmarkData (landmark, fid);
+
 		}
 		return (m_face_render->RenderFrame(QueryImage(PXCImage::IMAGE_TYPE_COLOR)) );
 	}
